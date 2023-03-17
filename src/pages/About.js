4f15819f55fed90f1c26aa9013ce8520948005d2
Ewelina Amrogowicz <ewelina.amrogowicz@gmail.com>
2023-03-17 14:36:04 +0000

    update css styles

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,45 +13,37 @@ import Grid from "@mui/material/Unstable_Grid2";
 function About() {
   return (
     <Grid container spacing={{ xs: 4, md: 8 }} columns={{ xs: 4, md: 12 }}>
-      <Grid xs={4}>
+      <Grid xs={6}>
         <Photo
           photo={process.env.PUBLIC_URL + "/ewelinastudio-painting.jpg"}
           alt="Ewelina in her home studio painting"
           look="imgLg"
         />
       </Grid>
-      <Grid xs={8}>
+      <Grid xs={6}>
         <Section>
           <Container fixed>
-            <Stack
-              direction={{ sm: "column", md: "row" }}
-              sx={{ width: "100%" }}
-              spacing={{ xs: 1, sm: 2, md: 4 }}
-              justifyContent="center"
-              alignItems="center"
-            >
-              <Box sx={{ display: "flex", flexWrap: "wrap" }}>
-                <TitleH2 text="About Me" />
-                <SubTitle
-                  text="Predominantly a landscape painter, Ewelina Amrogowicz is a
+            <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+              <TitleH2 text="About Me" />
+              <SubTitle
+                text="Predominantly a landscape painter, Ewelina Amrogowicz is a
                 Polish artist currently living in London."
-                />
-                <Paragraph
-                  text="Ewelina's focus on the natural world is stimulated by her passion
+              />
+              <Paragraph
+                text="Ewelina's focus on the natural world is stimulated by her passion
               for the environment and an interest in escapism. She dedicates
               herself to different topics and motives, but remain true to the
               photorealistic painting style. Currently expand her creative side
               in Web Development."
-                />
+              />
 
-                <CustomButton
-                  name="Download CV"
-                  link="https://www.canva.com/design/DAFbUzaKong/i-Ic--PXF2yApyzRGpZgPA/view?utm_content=DAFbUzaKong&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
-                  class="btnLight"
-                  target="_blank"
-                />
-              </Box>
-            </Stack>
+              <CustomButton
+                name="Download CV"
+                link="https://www.canva.com/design/DAFbUzaKong/i-Ic--PXF2yApyzRGpZgPA/view?utm_content=DAFbUzaKong&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
+                class="btnLight"
+                target="_blank"
+              />
+            </Box>
           </Container>
         </Section>
       </Grid>
